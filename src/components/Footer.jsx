8b35fc5ece8732a489c67d211469b523209b944c
Rohan Das <42172455+RohanDas28@users.noreply.github.com>
2024-01-26 21:03:22 +0530

    Fixed Link in Footer 💀

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <span className="text-2xl">&lt;<span className="text-yellow-400">Typer</span>Op/&gt;</span>
             {/* Footer Text with Link */}
             <span className="text-2xl text-gray-400 sm:py-2 sm:mt-0 mt-4">
-              Created with ❤️ by <a href="https://rohandas.github.io" className="text-yellow-500" target="_blank" rel="noopener noreferrer">Rohan Das</a>
+              Created with ❤️ by <a href="https://rohandas28.github.io" className="text-yellow-500" target="_blank" rel="noopener noreferrer">Rohan Das</a>
             </span>
           </span>
         </div>
